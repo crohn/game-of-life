0d619f126a7077d085637dcef8c2dee9449df177
Davide Giuseppe Monaco <monaco.dg@gmail.com>
2025-07-24 21:14:54 +0200

    avoid reallocating frame every cycle

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use std::{thread::sleep, time::Duration};
-
 use game_of_life::{
     cell::Cell,
     coords::Coords,
@@ -8,36 +6,19 @@ use game_of_life::{
 };
 
 fn main() -> Result<(), std::io::Error> {
-    let mut state = State::new(80, 25);
+    let cols = 80;
+    let rows = 25;
+    let mut state = State::new(cols, rows);
+    let mut frame = Frame::new(cols, rows, 10);
 
-    // state.set_cell(Coords::new(5, 3), Cell::Alive);
-    // state.set_cell(Coords::new(6, 4), Cell::Alive);
-    // state.set_cell(Coords::new(4, 5), Cell::Alive);
-    // state.set_cell(Coords::new(5, 5), Cell::Alive);
-    // state.set_cell(Coords::new(6, 5), Cell::Alive);
     state.set_cell(Coords::new(4, 5), Cell::Alive);
     state.set_cell(Coords::new(4, 6), Cell::Alive);
     state.set_cell(Coords::new(4, 7), Cell::Alive);
     state.set_cell(Coords::new(3, 6), Cell::Alive);
     state.set_cell(Coords::new(5, 7), Cell::Alive);
 
-    // state.set_cell(Coords::new(30, 30), Cell::Alive);
-    // state.set_cell(Coords::new(31, 30), Cell::Alive);
-    // state.set_cell(Coords::new(32, 30), Cell::Alive);
-    // state.set_cell(Coords::new(32, 29), Cell::Alive);
-    // state.set_cell(Coords::new(31, 28), Cell::Alive);
-
-    // loop {
-    //     let frame = Frame::new(&mut state, 10);
-    //     println!("\x1b[3J\x1b[H\x1b[2J");
-    //     println!("{}", frame.to_ascii());
-    //     state.next();
-    //     sleep(Duration::from_millis(100));
-    // }
-
     loop {
-        let mut frame = Frame::new(&mut state, 10);
-        render::render_kitty(&mut frame)?;
+        render::render_kitty(&mut frame, &state)?;
         state.next();
     }
 }
--- src/render.rs
+++ src/render.rs
@@ -3,35 +3,30 @@ use std::{io::Write, str};
 
 const CELL_BYTES: usize = 3;
 
-pub struct Frame<'a> {
+pub struct Frame {
     buffer: Vec<u8>,
     chunk_alive: Vec<u8>,
     cols: usize,
+    rows: usize,
     scale: usize,
-    state: &'a mut State,
 }
 
-impl<'a> Frame<'a> {
-    pub fn new(state: &'a mut State, scale: usize) -> Self {
-        let cols = state.cols;
-        let rows = state.rows;
-
+impl Frame {
+    pub fn new(cols: usize, rows: usize, scale: usize) -> Self {
         Frame {
             buffer: vec![0x00; cols * rows * CELL_BYTES * scale * scale],
             chunk_alive: vec![0xff; CELL_BYTES * scale],
             cols,
+            rows,
             scale,
-            state,
         }
     }
 
-    pub fn to_ascii(&self) -> String {
+    pub fn to_ascii(&self, state: &State) -> String {
+        let &Self { cols, rows, .. } = self;
         let mut cursor: usize = 0;
 
-        let cols = self.state.cols;
-        let rows = self.state.rows;
-
-        let bytes = self.state.to_ascii();
+        let bytes = state.to_ascii();
 
         let frame_capacity = cols * rows + rows;
         let mut frame = String::with_capacity(frame_capacity);
@@ -46,11 +41,13 @@ impl<'a> Frame<'a> {
         frame
     }
 
-    pub fn to_rgb(&mut self) -> String {
+    pub fn to_rgb(&mut self, board: &Vec<Cell>) -> String {
+        self.buffer.fill(0x00);
+
         let chunk_len = self.chunk_alive.len();
         let mut buf_cur = 0;
 
-        for row in self.state.board.chunks_exact(self.cols) {
+        for row in board.chunks_exact(self.cols) {
             let row_cur = buf_cur;
 
             for cell in row {
@@ -73,22 +70,17 @@ impl<'a> Frame<'a> {
     }
 }
 
-pub fn render_kitty(frame: &mut Frame) -> Result<(), std::io::Error> {
-    let &mut State {
-        cols,
-        rows,
-        iteration,
-        ..
-    } = frame.state;
+pub fn render_kitty(frame: &mut Frame, state: &State) -> Result<(), std::io::Error> {
+    let &mut Frame { cols, rows, .. } = frame;
 
     let id = "123";
     let depth: u8 = 24;
     let width = cols * frame.scale;
     let height = rows * frame.scale;
-    let payload = frame.to_rgb();
+    let payload = frame.to_rgb(&state.board);
     let quiet: u8 = 2;
 
-    if iteration == 0 {
+    if state.iteration == 0 {
         print!(
             "\x1b_Ga=T,f={},s={},v={},i={},q={};{}\x1b\\",
             depth, width, height, id, quiet, payload
@@ -101,7 +93,7 @@ pub fn render_kitty(frame: &mut Frame) -> Result<(), std::io::Error> {
         );
     }
 
-    if iteration % 30 == 0 {
+    if state.iteration % 30 == 0 {
         std::io::stdout().flush()?;
     }
 
