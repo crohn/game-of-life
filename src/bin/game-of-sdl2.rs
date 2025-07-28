f993bbf72459d07f5723c50b8959b786fe9ba48c
Davide Giuseppe Monaco <monaco.dg@gmail.com>
2025-07-28 19:39:23 +0200

    sdl2 rendering, separation

--- src/bin/game-of-sdl2.rs
+++ src/bin/game-of-sdl2.rs
@@ -1,13 +1,15 @@
 use game_of_life::{
     core::{Cell, Coords, State},
-    render::sdl,
+    render::sdl::{Game, SdlContext},
 };
 
 const COLS: u32 = 80;
 const ROWS: u32 = 25;
+const SCALE: u32 = 10;
 
 fn main() -> Result<(), String> {
     let mut state = State::new(COLS, ROWS);
+    let sdl_context = SdlContext::new(COLS, ROWS, SCALE)?;
 
     state.set_cell(Coords { x: 40, y: 39 }, Cell::Alive);
     state.set_cell(Coords { x: 40, y: 40 }, Cell::Alive);
@@ -15,5 +17,6 @@ fn main() -> Result<(), String> {
     state.set_cell(Coords { x: 39, y: 40 }, Cell::Alive);
     state.set_cell(Coords { x: 41, y: 41 }, Cell::Alive);
 
-    sdl::run(&mut state, 10)
+    let mut game = Game::new(sdl_context, state);
+    game.run()
 }
--- src/render/sdl.rs
+++ src/render/sdl.rs
@@ -1,4 +1,7 @@
-use sdl2::{event::Event, keyboard::Keycode, pixels::Color, rect::Rect};
+use sdl2::{
+    Sdl, TimerSubsystem, event::Event, keyboard::Keycode, pixels::Color, rect::Rect,
+    render::Canvas, video::Window,
+};
 
 use crate::core::{Cell, State, coords_from_index};
 
@@ -8,66 +11,116 @@ const COLOR_ALIVE: Color = Color::RGB(0xff, 0xff, 0xff);
 const FPS: u32 = 30;
 const FRAME_DURATION_MS: u32 = 1000 / FPS;
 
-pub fn run(state: &mut State, scale: u32) -> Result<(), String> {
-    let cols = state.cols;
-    let rows = state.rows;
+pub struct SdlContext {
+    sdl_context: Sdl,
+    canvas: Canvas<Window>,
+    timer: TimerSubsystem,
 
-    let sdl_context = sdl2::init()?;
+    scale: u32,
+}
 
-    let video = sdl_context.video()?;
-    let timer = sdl_context.timer()?;
+impl SdlContext {
+    pub fn new(width: u32, height: u32, scale: u32) -> Result<Self, String> {
+        let sdl_context = sdl2::init()?;
 
-    let window = video
-        .window("game-of-sdl2", cols * scale, rows * scale)
-        .position_centered()
-        .build()
-        .map_err(|e| e.to_string())?;
+        let timer = sdl_context.timer()?;
+        let video = sdl_context.video()?;
 
-    let mut canvas = window
-        .into_canvas()
-        .present_vsync()
-        .build()
-        .map_err(|e| e.to_string())?;
+        let window = video
+            .window("game-of-sdl2", width * scale, height * scale)
+            .position_centered()
+            .build()
+            .map_err(|e| e.to_string())?;
 
-    let mut event_pump = sdl_context.event_pump()?;
-    let mut rect = Rect::new(0, 0, scale, scale);
-    'running: loop {
-        let frame_start = timer.ticks();
+        let canvas = window
+            .into_canvas()
+            .present_vsync()
+            .build()
+            .map_err(|e| e.to_string())?;
 
-        for event in event_pump.poll_iter() {
-            match event {
-                Event::Quit { .. }
-                | Event::KeyDown {
-                    keycode: Some(Keycode::Escape),
-                    ..
-                } => break 'running,
-                _ => {}
+        Ok(SdlContext {
+            sdl_context,
+            canvas,
+            timer,
+            scale,
+        })
+    }
+}
+
+pub struct Game {
+    running: bool,
+    sdl_context: SdlContext,
+    state: State,
+}
+
+impl Game {
+    pub fn new(sdl_context: SdlContext, state: State) -> Self {
+        Game {
+            running: false,
+            sdl_context,
+            state,
+        }
+    }
+
+    pub fn run(&mut self) -> Result<(), String> {
+        let scale = self.sdl_context.scale;
+
+        let mut event_pump = self.sdl_context.sdl_context.event_pump()?;
+        let mut rect = Rect::new(0, 0, scale, scale);
+        'running: loop {
+            let frame_start = self.sdl_context.timer.ticks();
+
+            for event in event_pump.poll_iter() {
+                match event {
+                    Event::Quit { .. }
+                    | Event::KeyDown {
+                        keycode: Some(Keycode::Escape),
+                        ..
+                    } => break 'running,
+                    Event::KeyDown {
+                        keycode: Some(Keycode::Space),
+                        ..
+                    } => self.running = !self.running,
+                    _ => {}
+                }
+            }
+
+            self.update();
+            self.render(&mut rect)?;
+
+            let frame_time = self.sdl_context.timer.ticks() - frame_start;
+            if frame_time < FRAME_DURATION_MS {
+                self.sdl_context.timer.delay(FRAME_DURATION_MS - frame_time);
             }
         }
 
-        canvas.set_draw_color(COLOR_DEAD);
-        canvas.clear();
+        Ok(())
+    }
 
-        canvas.set_draw_color(COLOR_ALIVE);
-        for (i, cell) in state.curr.iter().enumerate() {
+    fn update(&mut self) {
+        if self.running {
+            self.state.next()
+        }
+    }
+
+    fn render(&mut self, rect: &mut Rect) -> Result<(), String> {
+        let cols = self.state.cols;
+        let scale = self.sdl_context.scale;
+
+        self.sdl_context.canvas.set_draw_color(COLOR_DEAD);
+        self.sdl_context.canvas.clear();
+
+        self.sdl_context.canvas.set_draw_color(COLOR_ALIVE);
+        for (i, cell) in self.state.curr.iter().enumerate() {
             if matches!(cell, Cell::Alive) {
                 let coords = coords_from_index(i, cols);
                 rect.x = coords.x * scale as i32;
                 rect.y = coords.y * scale as i32;
 
-                canvas.fill_rect(rect)?;
+                self.sdl_context.canvas.fill_rect(*rect)?;
             }
         }
 
-        canvas.present();
-
-        state.next();
-
-        let frame_time = timer.ticks() - frame_start;
-        if frame_time < FRAME_DURATION_MS {
-            timer.delay(FRAME_DURATION_MS - frame_time);
-        }
+        Ok(self.sdl_context.canvas.present())
     }
-
-    Ok(())
 }
